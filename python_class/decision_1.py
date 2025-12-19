aafce6e6fa16128c4bae41cd7034b3caa9b9fe98
Lee Gaeun <141293346+baekdmyng@users.noreply.github.com>
2025-12-19 15:22:07 +0900

    Create decision_1.py

---
+++ python_class/decision_1.py
@@ -0,0 +1,17 @@
+a = int(input("a: "))
+b = int(input("b: "))
+y = 0
+
+if a > 0:
+    y += 1
+
+else:
+    print("안녕 철수!")
+
+if y <=0:
+    print(y+12)
+    
+else:
+    print(y + 5)
+    y += 1
+    print(y)
