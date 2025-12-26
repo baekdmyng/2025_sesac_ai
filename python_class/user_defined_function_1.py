c030947c121161df0c47e1798e9513b1f6bc630c
Lee Gaeun <141293346+baekdmyng@users.noreply.github.com>
2025-12-26 16:50:22 +0900

    무지개

---
+++ python_class/user_defined_function_1.py
@@ -0,0 +1,10 @@
+def display(color):
+    msg = "무지개에" + color +  "색이 있어요!"
+    return(msg)
+print(display("빨간"))
+print(display("주황"))
+print(display("노란"))
+print(display("초록"))
+print(display("파랑"))
+print(display("남색"))
+print(display("보라"))
